d05515470c7a25bb87873b586992e309e17f3a3d
Javi <43217710+Javi801@users.noreply.github.com>
2020-10-22 02:03:47 -0300

    walking and get_hit

--- Enemy.gd
+++ Enemy.gd
@@ -1,16 +1,87 @@
 extends RigidBody2D
 
 
-export var min_speed = 150  # Minimum speed range.
-export var max_speed = 250  # Maximum speed range.
+var life = 500
+var alive = true
+
+var linear_vel = Vector2()
+var speed = 0
+var min_speed = 10
+var max_speed = 20
+var dir = Vector2(-1,0)
+
+var falling = false
+var flaying = false
+var on_floor = false
+
+var gravity = 800
+var gliding_gravity = gravity/2
+var jump_friction = 0.2
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = "walk"
+	speed = rand_range(min_speed, max_speed)
+	#set_gravity_scale(gravity)
+	
+	
+func _physics_process(delta):
+	#on_floor = is_on_floor()	
+	if (position.y < 430) and (not falling and not flaying):
+		get_hit(Vector2(1,0))
+	
+	if not falling and not flaying:
+			# Enemy walk with this things
+			scale.x = -dir.x
+			scale.y = 1
+			$AnimatedSprite.animation = "walk"
+			linear_vel = dir * speed
+			set_linear_velocity(linear_vel)
+	
+	if flaying:
+		set_friction(jump_friction)
+		linear_vel.y += gliding_gravity * delta * 10
+		set_linear_velocity(linear_vel)
+		var vel = get_linear_velocity()
+		if vel.y >= 0:
+			flaying = false
+			falling = true
+			$AnimatedSprite.animation = "falling"
+	
+	if falling:
+		scale.y = -1
+		if linear_vel.y <= gravity:
+			linear_vel.y += gliding_gravity * delta * 10
+		var pos = position
+		if pos.y >= 430:
+			falling = false
+			pos.y = 435
+			linear_vel.y = 0
+			scale.y = 1
+		set_linear_velocity(linear_vel)
+		
 
+func get_hit(direction):
+	direction = direction.normalized()
+	var new_linear_vel = Vector2()
+	if direction.x < 0:
+		dir = Vector2(1,0)
+		new_linear_vel = Vector2(-1,0)
+	else:
+		dir = Vector2(-1,0)
+		new_linear_vel = Vector2(1,0)
+	scale.x = -new_linear_vel.x
+	new_linear_vel.x = new_linear_vel.x*100
+	new_linear_vel.y = -gravity*2
+	linear_vel = new_linear_vel
+	set_linear_velocity(linear_vel)
+	$AnimatedSprite.animation = "get_hit"
+	flaying = true
+	falling = false
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	#apply_gravity(delta)
+	pass
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -2,13 +2,12 @@ extends Node2D
 export (PackedScene) var Enemy
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var new_enemy_position = Vector2(500,435)
+var enemies = 4
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
 	new_game()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,25 +15,34 @@ func _ready():
 #	pass
 
 func game_over():
-	$EnemyTimer.stop()
-	
+	$EnemyTimer.stop()	
 
 func new_game():
-	$StartTimer.start()
+	go_enemy()
 
-func _on_StartTimer_timeout():
-	$EnemyTimer.start()
+func go_enemy():
+	$EnemyTimer.start(3) # para 3 segundos
+	yield($EnemyTimer, "timeout")
+	new_scarecrow()
+	if enemies>0:
+		go_enemy()
 	
+	
+func new_scarecrow():
+	var scarecrow = Enemy.instance()
+	#var screensize = get_viewport().get_visible_rect().size
+	new_enemy_position.x += 200
+	scarecrow.position = new_enemy_position
+	add_child(scarecrow)
+	enemies -= 1
+		
 func _on_EnemyTimer_timeout():
 	# Choose a random location on Path2D.
 	$EnemyPath/EnemySpawnLocation.offset = randi()
 	# Create a scarecrow instance and add it to the scene.
 	var scarecrow = Enemy.instance()
 	add_child(scarecrow)
-	# Set the scarecrow's direction perpendicular to the path direction.
-	var direction = $EnemyPath/EnemySpawnLocation.rotation + PI
 	# Set the mob's position to a random location.
 	scarecrow.position = $EnemyPath/EnemySpawnLocation.position
 	# Set the velocity (speed & direction).
 	scarecrow.linear_velocity = Vector2(rand_range(scarecrow.min_speed, scarecrow.max_speed), 0)
-	scarecrow.linear_velocity = scarecrow.linear_velocity.rotated(direction)
