ccb5049f0c05b3fa41b6816bb321cebfd6b73267
Pablo Aliaga <pablooaliaga@gmail.com>
2020-10-30 03:33:18 -0300

    Added dash icon

--- interface/Gui.gd
+++ interface/Gui.gd
@@ -6,9 +6,12 @@ onready var heart_2 = $MarginContainer/NinePatchRect/Heart_2
 onready var heart_3 = $MarginContainer/NinePatchRect/Heart_3
 onready var hearts = [heart_1, heart_2, heart_3]
 
+onready var dash_icon = $MarginContainer3/Dash_icon
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_lives(max_hearts)
+	dash_available()
 	
 func update_lives(number_of_lives):
 	assert(number_of_lives >= 0 and number_of_lives <= 3)
@@ -17,6 +20,12 @@ func update_lives(number_of_lives):
 			hearts[i].show()
 		else:
 			hearts[i].hide()
+			
+func dash_on_cooldwon():
+	dash_icon.modulate = Color(0.1,0.1,0.1)
+	
+func dash_available():
+	dash_icon.modulate = Color (1,1,1)
 			
 		
 	
