d4ede57cb9f5d48203c3c37140df30a869b9f114
Javi <43217710+Javi801@users.noreply.github.com>
2020-10-30 01:21:13 -0300

    trying to fix enemy bug

--- Enemy.gd
+++ Enemy.gd
@@ -7,7 +7,7 @@ var alive = true
 var linear_vel = Vector2()
 var speed = 0
 var min_speed = 10
-var max_speed = 20
+var max_speed = 30
 var dir = Vector2(-1,0)
 
 var falling = false
@@ -17,6 +17,8 @@ var on_floor = false
 var gravity = 800
 var gliding_gravity = gravity/2
 var jump_friction = 0.2
+
+var facing_left = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -32,13 +34,16 @@ func is_on_floor():
 	return false
 	
 func _physics_process(delta):
-	on_floor = is_on_floor()	
+	on_floor = is_on_floor()
 	
 	if not falling and not flaying:
-		#if scale.y != 1:
-		#	scale.y = 1
+		if facing_left and dir.x == 1:
+			facing_left = false
+			scale.x = -1
+		elif (not facing_left) and dir.x == -1:
+			facing_left = true
+			scale.x = -1
 		#Enemy walk with this things
-		scale.x = -dir.x
 		$AnimatedSprite.animation = "walk"
 		linear_vel = dir * speed
 		set_linear_velocity(linear_vel)
@@ -54,14 +59,12 @@ func _physics_process(delta):
 			#$AnimatedSprite.animation = "falling"
 	
 	if falling:
-		#scale.y = -1
 		if linear_vel.y <= gravity:
 			linear_vel.y += gliding_gravity * delta * 10
 		if is_on_floor():
 			falling = false
 			position.y = 435
 			linear_vel.y = 0
-			#scale.y = 1
 		set_linear_velocity(linear_vel)
 		
 
@@ -69,12 +72,17 @@ func get_hit(direction):
 	direction = direction.normalized()
 	var new_linear_vel = Vector2()
 	if direction.x < 0:
+		if facing_left:
+			facing_left = false
+			scale.x = -1
 		dir = Vector2(1,0)
 		new_linear_vel = Vector2(-1,0)
 	else:
+		if not facing_left:
+			facing_left = true 
+			scale.x = -1
 		dir = Vector2(-1,0)
 		new_linear_vel = Vector2(1,0)
-	scale.x = -new_linear_vel.x
 	new_linear_vel.x = new_linear_vel.x*500
 	new_linear_vel.y = -gravity/2
 	linear_vel = new_linear_vel
