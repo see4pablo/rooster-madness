7ae4d07117f31a9ca32df31d8b5186228e7b4cb3
lecaroos <pancho.lecaroos@gmail.com>
2021-01-06 22:48:20 -0300

    Menu and credits added

---
+++ title.gd
@@ -0,0 +1,24 @@
+extends Node
+
+
+
+func _ready():
+	 $MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+
+
+
+func _physics_process(delta):
+	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton.is_hovered()==true:
+		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton.grab_focus()
+		
+	if $MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.is_hovered()==true:
+		$MarginContainer/VBoxContainer/VBoxContainer/TextureButton2.grab_focus()
+	
+
+
+func _on_TextureButton_pressed():
+	get_tree().change_scene("res://Game.tscn")
+
+
+func _on_TextureButton2_pressed():
+	get_tree().change_scene("res://credits.tscn")
